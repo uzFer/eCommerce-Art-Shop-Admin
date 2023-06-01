2d2c791c9a5ae555febd264f676de9cd60bef0d7
uzFer <109243682+uzFer@users.noreply.github.com>
2023-06-01 15:28:27 -0400

    new product page is done

--- src/pages/products/new.js
+++ src/pages/products/new.js
@@ -1,14 +1,28 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 export default function NewProduct() {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+
     return (
         <Layout>
-            <h1 >New Product</h1>
+            <h1 className="text-blue-900 mb-2 text-xl">New Product</h1>
+            <label>Product Name</label>
             <input type="text" 
-                placeholder="Product Name">
-            </input>
-            <textarea placeholder="Product Description">
+                placeholder="Product Name"
+                value={name}
+                onChange={e =>(e.target.value)}
+            />
+            <label>Brief Description of Product</label>
+            <textarea placeholder="Description">
             </textarea>
+            <label>Price (in CAD)</label>
+            <input type="number" 
+                placeholder="Price"
+            />
+            <button className="primary-btn">Save</button>
         </Layout>
     );
 }
