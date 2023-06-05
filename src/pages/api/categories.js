975a1bfdeda2f5b73f0c6d4d9727defc50aa2876
uzFer <109243682+uzFer@users.noreply.github.com>
2023-06-05 01:21:07 -0400

    :((((((((((((((((((((((((((((((((((((

--- src/pages/api/categories.js
+++ src/pages/api/categories.js
@@ -1,12 +1,17 @@
+import clientPromise from "@/lib/mongodb";
 import mongooseConnect from "@/lib/mongoose";
 import { Category } from "@/models/Category";
+import mongoose from "mongoose";
 
 export default async function handle(req, res) {
     const {method} = req;
+    await mongooseConnect();
 
     if(method === 'POST') {
-        const {name} = req.body;
-        const categoryDoc = await Category.create({name});
-        res.json(categoryDoc);
+        const {name} = req.body; 
+        const productDoc = await Category.create({
+            name
+        });
+        res.json(productDoc)
     }
 }
